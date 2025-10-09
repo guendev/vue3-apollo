c08d4f5db2ccf00002b696f8e20d5c0b56e8b592
guen <heloo@guen.dev>
2025-10-10 02:38:59 +0900

    Integrate Apollo Client and GraphQL functionality:
    - Add `apolloPlugin` setup in `@vue3-apollo/core` and integrate with the web package.
    - Update `tsup.config.ts` with enhanced ES2020 build settings and defined module conditions.
    - Refactor `packages/web/src/App.vue` with Apollo Query and Mutation logic.
    - Adjust `package.json` files for `@vue3-apollo/core` and `@vue3-apollo/operations` to include appropriate dependencies and configurations.
    - Refine ESLint and TypeScript configurations.

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -2,18 +2,8 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     clean: true,
-    dts: {
-        compilerOptions: {
-            composite: false,
-            customConditions: []
-        },
-        entry: 'src/index.ts',
-        resolve: true
-    },
+    dts: true,
     entry: ['src/index.ts'],
-    esbuildOptions(options) {
-        options.conditions = ['module', 'import', 'require']
-    },
     external: ['vue', '@apollo/client', 'graphql'],
     format: ['esm', 'cjs'],
     minify: false,
