6b7de8d06a6075d64c909df92038dd9fa6c331bc
guen <heloo@guen.dev>
2025-10-10 00:29:03 +0900

    Simplify `build.config.ts` by formatting and removing unused `outDir` field.

--- core/build.config.ts
+++ core/build.config.ts
@@ -3,15 +3,12 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
     clean: true,
     declaration: true,
-    entries: [
-        'core/src/index'
-    ],
+    entries: ['core/src/index'],
     externals: [
         'vue',
         '@apollo/client',
         'graphql'
     ],
-    outDir: 'dist',
     rollup: {
         emitCJS: true,
         esbuild: {
