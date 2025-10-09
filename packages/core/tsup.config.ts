5e40eaf84a1307b871db35febfc09b8cccc10f12
guen <heloo@guen.dev>
2025-10-09 19:48:15 +0900

    Simplify `tsup` configuration: update `dts` to shorthand, remove unused `esbuildOptions`.

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -2,26 +2,11 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     clean: true,
-    dts: {
-        compilerOptions: {
-            composite: false,
-            declaration: true,
-            declarationMap: true,
-            skipLibCheck: true
-        },
-        resolve: true
-    },
+    dts: true,
     entry: ['src/index.ts'],
-    esbuildOptions(options) {
-        options.mangleProps = undefined
-        options.reserveProps = undefined
-        options.mangleQuoted = false
-        options.keepNames = true
-    },
     external: ['vue', '@apollo/client', 'graphql', '@vueuse/core'],
     format: ['esm', 'cjs'],
     minify: false,
-    onSuccess: 'echo "Build completed successfully!"',
     outDir: 'dist',
     platform: 'neutral',
     shims: true,
