07c5eb3c69b3e0f942792e4c99b75d052c6ecab7
guen <heloo@guen.dev>
2025-10-09 15:53:21 +0900

    Fix typos in warning messages in `useSubscription` and `useQuery` logs.

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     clean: true,
-    dts: {
-        resolve: true
-    },
+    dts: true,
     entry: ['src/index.ts'],
     format: ['esm', 'cjs'], // dual build
     minify: false,
     skipNodeModulesBundle: true,
     sourcemap: true,
-    splitting: false, // lib nhỏ, tránh split rườm rà
+    splitting: true, // lib nhỏ, tránh split rườm rà
     target: 'es2020',
     treeshake: true
 })
