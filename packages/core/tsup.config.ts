4f83e8ebc5bf3bff3ed5061d105563e2576505ff
guen <heloo@guen.dev>
2025-10-09 19:45:34 +0900

    Refine `tsup` configuration: adjust `dts` options, enable splitting, update target to `es2022`, and add post-build success message. Remove TypeScript-only build step from `package.json`.

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -2,14 +2,31 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     clean: true,
-    dts: { entry: 'src/index.ts' },
+    dts: {
+        compilerOptions: {
+            composite: false,
+            declaration: true,
+            declarationMap: true,
+            skipLibCheck: true
+        },
+        resolve: true
+    },
     entry: ['src/index.ts'],
+    esbuildOptions(options) {
+        options.mangleProps = undefined
+        options.reserveProps = undefined
+        options.mangleQuoted = false
+        options.keepNames = true
+    },
     external: ['vue', '@apollo/client', 'graphql', '@vueuse/core'],
     format: ['esm', 'cjs'],
     minify: false,
+    onSuccess: 'echo "Build completed successfully!"',
     outDir: 'dist',
+    platform: 'neutral',
+    shims: true,
     sourcemap: true,
-    splitting: false,
-    target: 'es2020',
+    splitting: true,
+    target: 'es2022',
     treeshake: true
 })
