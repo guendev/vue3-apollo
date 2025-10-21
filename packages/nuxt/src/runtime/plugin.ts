0fb2a640968ece3fd4ec938a606bc4bd9dd4d605
guen <heloo@guen.dev>
2025-10-21 11:28:32 +0700

    fix(runtime): rename `globalConfig` to `apolloConfig` in Apollo client setup

--- packages/nuxt/src/runtime/plugin.ts
+++ packages/nuxt/src/runtime/plugin.ts
@@ -8,18 +8,18 @@ import { createApolloClient } from './utils/createApolloClient'
 
 export default defineNuxtPlugin(async (nuxtApp) => {
     const runTimeconfig = nuxtApp.$config
-    const globalConfig = runTimeconfig.public.apollo
+    const apolloConfig = runTimeconfig.public.apollo
 
-    if (!globalConfig?.clients) {
+    if (!apolloConfig?.clients) {
         throw new Error('[vue3-apollo] No Apollo clients configured')
     }
 
     const apolloClients: Record<string, ApolloClient> = {}
 
-    for (const [clientId, config] of Object.entries(globalConfig.clients)) {
+    for (const [clientId, config] of Object.entries(apolloConfig.clients)) {
         apolloClients[clientId] = await createApolloClient({
             clientId,
-            config: defu(config, omit(globalConfig, ['clients', 'autoImports'])),
+            config: defu(config, omit(apolloConfig, ['clients', 'autoImports'])),
             nuxtApp
         })
     }
